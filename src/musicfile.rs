0e701154326ec028751acd62c771ac5151dd9d0c
jules <jules.maryt@ent.uvsq.fr>
2022-11-14 20:40:26 +0100

    scan fonctionnel, fichier annotés. problèmes : musicfiles doit écrire distinctement les metadatas dans le json (et de façon plus détaillés) + inclure les metadatas non propres au mp3.
    ajouter un argument non obligatoir qui se place avant (ou après) les deux autres pour la sauvegarde du résultat de la requète dans un fichier markdown.

--- src/musicfile.rs
+++ src/musicfile.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
 use id3::{Tag, TagLike};
-use serde::ser::{Serialize, Serializer, SerializeStruct};
+// use serde::ser::{Serialize, Serializer, SerializeStruct};
 use serde_json;
 
 // structure de base
@@ -16,27 +16,33 @@ impl MusicFile {
         // création d'un tag ID3 -> contient l'ensemble des metadatas spécifique aux fichiers mp3
         // !!! on obtient pas les metadata classiques -> a ajouter plus tard.
         let tag = Tag::read_from_path(path).unwrap();
+
         // tout une série de test pour arriver a écrire dans un json
         let mut newpath: PathBuf = PathBuf::new();
         newpath.push("src/test/testjson.json");
-        std::fs::write(newpath,serde_json::to_string(path).unwrap());
+        std::fs::write(&newpath, Self::generate_str(&path));
+        Self::read_tag(&tag);
+
         // retourne le constructeur
         MusicFile {
             path: path.to_path_buf(),
             metadata: tag,
         }
-
     }
-}
 
-// implémentation de la fonction serialize (pas nécessaire au final, surement supprimé dans la prochaine version)
-impl Serialize for MusicFile {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer, {
-        let mut state = serializer.serialize_struct("MusicFile", 2)?;
-        state.serialize_field("path", &self.path)?;
-        //state.serialize_field("metadata", &self.metadata)?;
-        state.end()
+    pub fn generate_str(path: &Path) -> String {
+        let mut mystring:String = String::new();
+        mystring.push_str("{\"path\": ");
+        mystring.push_str(&serde_json::to_string(path).unwrap());
+        mystring.push_str("}");
+        mystring
+    }
+
+    pub fn read_tag(tag:&Tag) {
+        println!("==================================");
+        println!("==================================");
+        println!("{}", tag.duration().unwrap());
+        println!("==================================");
+        println!("==================================");
     }
 }
