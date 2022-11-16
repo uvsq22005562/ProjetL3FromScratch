8d78a5eb2643522927c293d4fa7c86d90295c0f7
jules <jules.maryt@ent.uvsq.fr>
2022-11-16 14:30:55 +0100

    scan fonctionnel, fichier annotés. problèmes : musicfiles doit écrire distinctement les metadatas dans le json (et de façon plus détaillés) + inclure les metadatas non propres au mp3.
    ajouter un argument non obligatoir qui se place avant (ou après) les deux autres pour la sauvegarde du résultat de la requète dans un fichier markdown.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+use std::path::Path;
+use projet_L3_2::cli::Cli;
+use projet_L3_2::scan::scan;
+// fichier principal
+
+fn main() {
+    // récupération des entrées de l'user avec module cli
+    let args = Cli::parse();
+    println!("{:?}", args); // checkprint
+    // gestion du cas scan
+    if args.command == "scan" {
+        // création du vect contenant tout les fichiers supportés
+        let music_files = scan(&args.path);
+        // checkprints
+        for elm in music_files {
+            println!("----------------------------");
+            println!("{:?}", elm);
+            println!("----------------------------");
+        }
+    }
+}
